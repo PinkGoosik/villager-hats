2a2d42def3741f2ad7b0b2be7cac5e751b087852
PinkGoosik <pinkgoosik@vk.com>
2022-05-19 20:16:13 +0500

    huh

--- src/main/java/ru/pinkgoosik/villagerhats/item/VillagerHatItem.java
+++ src/main/java/ru/pinkgoosik/villagerhats/item/VillagerHatItem.java
@@ -1,10 +1,18 @@
 package ru.pinkgoosik.villagerhats.item;
 
-import dev.emi.trinkets.api.TrinketItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Wearable;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.village.VillagerProfession;
+import net.minecraft.world.World;
 
-public class VillagerHatItem extends TrinketItem {
+public class VillagerHatItem extends Item implements VillagerHat, Wearable {
     private final float size;
     private final double height;
     private final VillagerProfession profession;
@@ -23,19 +31,41 @@ public class VillagerHatItem extends TrinketItem {
         this.profession = profession;
     }
 
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+
+        if (user.getEquippedStack(EquipmentSlot.HEAD).isEmpty()) {
+            if(!world.isClient) {
+                user.equipStack(EquipmentSlot.HEAD, new ItemStack(stack.getItem(), 1));
+                stack.decrement(1);
+            }
+            else {
+                user.playSound(SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 1.0F, 1.0F);
+            }
+            return TypedActionResult.success(stack);
+        }
+        return TypedActionResult.fail(stack);
+    }
+
+    @Override
     public VillagerProfession getProfession() {
         return profession;
     }
 
+    @Override
     public String getHatName() {
         return profession.getId() + "_hat";
     }
 
+    @Override
     public float getSize() {
         return size;
     }
 
+    @Override
     public double getHeight() {
         return height;
     }
+
 }
